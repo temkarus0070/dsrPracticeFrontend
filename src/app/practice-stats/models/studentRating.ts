8a2ac06215bcec0f76ecd1c1ef5a4867bce090f3
artyom <pretkovartyom@gmail.com>
2022-07-05 11:43:05 +0300

    add programming language to students rating

--- src/app/practice-stats/models/studentRating.ts
+++ src/app/practice-stats/models/studentRating.ts
@@ -1,8 +1,10 @@
 import {Student} from "../../student/entities/student";
 import {Mark} from "../../practice-ticket/entity/mark";
+import {ProgrammingLanguage} from "../../practice-ticket/entity/programmingLanguage";
 
 export class StudentRating {
   student!: Student
   recommendToHire!: boolean
   finalMark!: Mark
+  programmingLanguage!: ProgrammingLanguage
 }
